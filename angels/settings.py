2c1d52eca6b3dd694fdbc920986588c762f00c6f
subikshan <k.subikshandevp@gmail.com>
2025-10-04 10:40:30 +0530

    Update settings.py

--- angels/settings.py
+++ angels/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--@ol#!m2b*bz_bs4d)o#%4(k0(gix^2paz^116)2*+fhqtgf)^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["195.250.21.17"]
+ALLOWED_HOSTS = ["195.250.21.17","angels-glamnglow.in","www.angels-glamnglow.in"]
 
 
 # Application definition
