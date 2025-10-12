6f65704528756d41986e04b762139210378a6183
subikshan <k.subikshandevp@gmail.com>
2025-10-12 13:21:10 +0530

    Update settings.py

--- angels/settings.py
+++ angels/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
-"""
+
 
 from pathlib import Path
 
