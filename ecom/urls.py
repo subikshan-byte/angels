36c72280cd9a499bf0149d01e5f411145da7eb8c
subikshan <k.subikshandevp@gmail.com>
2025-10-18 23:20:48 +0530

    Update urls.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('cart1/remove1/<int:item_id>/', views5.remove_from_cart1, name='remove_from_cart1'),
     path('buy/<slug:slug>/', views6.buy_now, name='buy_now'),
 path('payment/success/', views6.payment_success, name='payment_success'),
+    path('verify-order-otp/', views6.verify_order_otp, name='verify_order_otp'),
 path('cart/update/<int:cart_item_id>/', views6.update_cart_quantity, name='update_cart_quantity'),
 path('checkout/cart/', views7.cart_checkout, name='cart_checkout'),
 
