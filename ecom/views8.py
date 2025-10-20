fabae7f4b09daa22f76dcebd8d6bbf3adb97515a
subikshan <k.subikshandevp@gmail.com>
2025-10-20 15:12:07 +0530

    Update views8.py

--- ecom/views8.py
+++ ecom/views8.py
@@ -5,3 +5,5 @@ def about(request):
     return render(request,"about.html")
 def privacy(request):
     return render(request,"privacy.html")
+def paymentrazor(request):
+    return render(request,"payment.html")
