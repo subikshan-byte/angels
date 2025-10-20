84beca00892bdf1266ec7ca5219d0d62e19faba0
subikshan <k.subikshandevp@gmail.com>
2025-10-20 08:51:49 +0530

    Update urls.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('buy/<slug:slug>/', views6.buy_now, name='buy_now'),
 path('payment/success/', views6.payment_success, name='payment_success'),
     path('verify-order-otp/', views6.verify_order_otp, name='verify_order_otp'),
- 
+ path('payment/success/cart-checkout', views7.payment_success, name='payment_success1'),
 path('cart/update/<int:cart_item_id>/', views6.update_cart_quantity, name='update_cart_quantity'),
 path('checkout/cart/', views7.cart_checkout, name='cart_checkout'),
     path("send-checkout-otp/", views6.send_checkout_otp, name="send_checkout_otp"),
@@ -28,7 +28,7 @@ path('place-cod-order/', views6.place_cod_order, name='place_cod_order'),
 path('apply-coupon/', views7.apply_coupon, name='apply_coupon'),
 path('create-razorpay-order-cart/', views6.create_razorpay_order, name='create_razorpay_order1'),
 path("send-checkout-otp-cart/", views7.send_checkout_otp, name="send_checkout_otp1"),
-
+path('verify-order-otp-cart/', views7.verify_order_otp, name='verify_order_otp1'),
     # Payment success for cart
     path('payment/success-cart/', views7.payment_success_cart, name='cart_payment_success'),
     path("about",views8.about,name="about"),
