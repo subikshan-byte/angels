4d9d38ee626c23de0d4820bd9dcfd7d319ad6616
subikshan <k.subikshandevp@gmail.com>
2025-10-20 08:55:51 +0530

    Update urls.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -26,6 +26,7 @@ path('checkout/cart/', views7.cart_checkout, name='cart_checkout'),
 path('place-cod-order/', views6.place_cod_order, name='place_cod_order'),
     path('place-cod-order-cart/', views7.place_cod_order, name='place_cod_order1'),
 path('apply-coupon/', views7.apply_coupon, name='apply_coupon'),
+    
 path('create-razorpay-order-cart/', views6.create_razorpay_order, name='create_razorpay_order1'),
 path("send-checkout-otp-cart/", views7.send_checkout_otp, name="send_checkout_otp1"),
 path('verify-order-otp-cart/', views7.verify_order_otp, name='verify_order_otp1'),
