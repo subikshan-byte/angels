44c9a55aed9b64ea0a88fbd4f167983424ccf982
subikshan <k.subikshandevp@gmail.com>
2025-10-25 20:27:36 +0530

    Update signals.py the change in email

--- ecom/signals.py
+++ ecom/signals.py
@@ -32,11 +32,22 @@ def delete_old_productimage_file_on_update(sender, instance, **kwargs):
 
 
 # ---------------- PRODUCT MODEL ----------------
+
+
+# ecom/signals.py
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import UserProfile
+
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        from .models import UserProfile
         UserProfile.objects.create(user=instance)
 
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
 
 
