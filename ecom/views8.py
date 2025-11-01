d43f76c79269e7102b8a9fb5bf5ca67fb4d7b045
subikshan <k.subikshandevp@gmail.com>
2025-11-01 10:27:47 +0530

    Update views8.py

--- ecom/views8.py
+++ ecom/views8.py
@@ -3,12 +3,12 @@ def contact(request):
     log='0'
     if not request.user.is_authenticated:
         log='1'
-    return render(request,"contact.html")
+    return render(request,"contact.html",{"log":log,})
 def about(request):
     log='0'
     if not request.user.is_authenticated:
         log='1'
-    return render(request,"about.html")
+    return render(request,"about.html",{"log":log})
 def privacy(request):
     return render(request,"privacy.html")
 def paymentrazor(request):
