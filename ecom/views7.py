0c7ba695f9e79d669c198fda9ca369c8f3dbe0ec
subikshan <k.subikshandevp@gmail.com>
2025-11-01 10:32:24 +0530

    Update views7.py

--- ecom/views7.py
+++ ecom/views7.py
@@ -86,6 +86,21 @@ def cart_checkout(request):
     log='0'
     if not request.user.is_authenticated:
         log='1'
+    else:
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        cart_items = CartItem.objects.filter(cart=cart)
+
+        
+        for item in cart_items:
+            product = item.product
+            # attach quantity and subtotal
+            price+=item.quantity*product.price
+            product.quantity_in_cart = item.quantity
+            product.subtotal_in_cart = item.subtotal()
+            # attach first image url (or None if no image)
+            first_image = product.productimage_set.first()
+            product.image_url = first_image.image.url if first_image else ""
+            products.append(product)
 
     return render(request, "checkout1.html", {
         "items": items,
