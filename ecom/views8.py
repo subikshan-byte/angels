a442ee54635672cea4bf52a8403c4169b3d437b9
subikshan <k.subikshandevp@gmail.com>
2025-11-01 10:31:49 +0530

    Update views8.py

--- ecom/views8.py
+++ ecom/views8.py
@@ -3,11 +3,41 @@ def contact(request):
     log='0'
     if not request.user.is_authenticated:
         log='1'
+    else:
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        cart_items = CartItem.objects.filter(cart=cart)
+
+        
+        for item in cart_items:
+            product = item.product
+            # attach quantity and subtotal
+            price+=item.quantity*product.price
+            product.quantity_in_cart = item.quantity
+            product.subtotal_in_cart = item.subtotal()
+            # attach first image url (or None if no image)
+            first_image = product.productimage_set.first()
+            product.image_url = first_image.image.url if first_image else ""
+            products.append(product)
     return render(request,"contact.html",{"log":log,})
 def about(request):
     log='0'
     if not request.user.is_authenticated:
         log='1'
+    else:
+        cart, created = Cart.objects.get_or_create(user=request.user)
+        cart_items = CartItem.objects.filter(cart=cart)
+
+        
+        for item in cart_items:
+            product = item.product
+            # attach quantity and subtotal
+            price+=item.quantity*product.price
+            product.quantity_in_cart = item.quantity
+            product.subtotal_in_cart = item.subtotal()
+            # attach first image url (or None if no image)
+            first_image = product.productimage_set.first()
+            product.image_url = first_image.image.url if first_image else ""
+            products.append(product)
     return render(request,"about.html",{"log":log})
 def privacy(request):
     return render(request,"privacy.html")
