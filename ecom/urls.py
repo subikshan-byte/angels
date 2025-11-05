2985b7f63af5b57462bc99d339a5966e5b24eb3c
subikshan <k.subikshandevp@gmail.com>
2025-11-05 09:25:20 +0530

    Update urls.py

--- ecom/urls.py
+++ ecom/urls.py
@@ -46,7 +46,8 @@ path('verify-order-otp-cart/', views7.verify_order_otp, name='verify_order_otp1'
     path("sitemap",views8.sitemap,name="sitemap"),
     path("robots",views8.robots,name="robots"),
  
- path('apply-coupon/', views6.apply_coupon1, name='apply_coupon1'),
+path("apply-coupon1/", views6.apply_coupon1, name="apply_coupon1"),
+
 ]
 
 
