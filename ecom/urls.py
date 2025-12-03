9d6ed5ddee7c382081bc12bf2df08ab28eb0f8c3
subikshan <k.subikshandevp@gmail.com>
2025-12-03 21:29:26 +0530

    Update payment success path for cart

--- ecom/urls.py
+++ ecom/urls.py
@@ -38,7 +38,7 @@ path("send-checkout-otp-cart/", views7.send_checkout_otp, name="send_checkout_ot
 path('verify-order-otp-cart/', views7.verify_order_otp, name='verify_order_otp1'),
     # Payment success for cart
     path('payment/success-cart/', views6.payment_success, name='payment_success'),
-    path('payment/success-cart/', views7.payment_success_cart, name='payment_success1'),
+    path('payment/success-cart1/', views7.payment_success_cart, name='payment_success1'),
     path("about",views8.about,name="about"),
     path("contact",views8.contact,name="contact"),
     path("privacy",views8.privacy,name="privacy"),
